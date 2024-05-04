4eb251b34b204096434f7425da66a9ccf419d2f7
FrancoBarlocco <Franco.Barlocco@correo.ucu.edu.uy>
2024-05-03 22:27:31 -0300

    diagram

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import  utils
 
 # utils.print_results()
-utils.simulate_games(10, True)
-utils.simulate_games(100, False)
-utils.simulate_games(1000, False)
+utils.simulate_games(1000, True)
+utils.simulate_games(10000, False)
+utils.simulate_games(100000, False)
